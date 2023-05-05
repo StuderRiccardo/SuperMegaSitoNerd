b0a5be7a8ea09c287f96d8f1d08b60c6e8702acc
StuderRiccardo <studer.riccardo@iisgalvanimi.edu.it>
2023-05-05 11:57:38 +0000

    riprova

--- pokemon/src/app/sceptile/sceptile.component.ts
+++ pokemon/src/app/sceptile/sceptile.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-sceptile',
   templateUrl: './sceptile.component.html',
   styleUrls: ['./sceptile.component.css']
 })
 export class SceptileComponent {
-  sceptile = {
-    "small": "https://images.pokemontcg.io/xy1/1.png",
-    Image: "https://images.pokemontcg.io/xy7/85_hires.png"
+  data!: Object;
+  loading!: boolean;
+  o !:Observable<Object>;
+
+
+  constructor(public http: HttpClient) {}
+  makeRequest(): void {
+    console.log("here");
+    this.loading = true;
+    this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
+    this.o.subscribe(this.getData);
+  }
+  getData = (d : Object) =>
+  {
+    this.data = new Object(d);
+    this.loading = false;
+  }
+
+  makeCompactPost(): void{
+    this.loading = true;
+    this.http
+      .post('https://jsonplaceholder.typicode.com/posts',
+        JSON.stringify({
+          body: 'bar',
+          title: 'foo',
+          userId: 1
+        })
+        )
+        .subscribe(data => {
+          this.data = data;
+          this.loading = false;
+        });
   }
 
 }
